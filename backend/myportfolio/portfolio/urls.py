07e619852f2aebddf62d22632844b14e706fea07
Szilveszter <nmszilveszter@gmail.com>
2025-07-04 09:08:32 +0200

    finsihed url path

--- backend/myportfolio/portfolio/urls.py
+++ backend/myportfolio/portfolio/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, CertificationViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'certifications', CertificationViewSet)
+
 
 urlpatterns = [
-   
+   path('api/', include(router.urls)),
 ]
