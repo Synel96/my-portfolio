972883d46fe42a5019f1eb82c12839e6b7df1cb6
Szilveszter <nmszilveszter@gmail.com>
2025-07-04 11:42:25 +0200

    worikng projects cards with fetch

--- frontend/my-portfolio/src/services/projectService.js
+++ frontend/my-portfolio/src/services/projectService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://127.0.0.1:8000/portfolio/api/projects/";
-export const getProjects = async () => {
+
+export const fetchProjects = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/projects/`);
+    const response = await axios.get(API_BASE_URL);
     return response.data;
   } catch (error) {
     console.error("Failed to fetch projects:", error);
